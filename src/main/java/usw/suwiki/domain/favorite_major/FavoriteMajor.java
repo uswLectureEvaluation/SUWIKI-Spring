cefe11794468d70ee7133dbb9ddc31a235318c0d
JIWEON-JEONG <dlektl6@naver.com>
2022-05-03 22:53:48 +0900

    [ADD] new dentity favorite_major

---
+++ src/main/java/usw/suwiki/domain/favorite_major/FavoriteMajor.java
@@ -0,0 +1,12 @@
+package usw.suwiki.domain.favorite_major;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class FavoriteMajor {
+}
