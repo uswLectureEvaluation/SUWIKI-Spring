8893d645268fface0a00ec62a6d0bb19de3a94fe
JIWEON-JEONG <dlektl6@naver.com>
2022-05-04 23:05:57 +0900

    [ADD] Domain FavoriteMajor User domain

--- src/main/java/usw/suwiki/domain/favorite_major/FavoriteMajor.java
+++ src/main/java/usw/suwiki/domain/favorite_major/FavoriteMajor.java
@@ -2,11 +2,21 @@ package usw.suwiki.domain.favorite_major;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import usw.suwiki.domain.user.User;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class FavoriteMajor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //Auto Increment
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_idx")
+    private User user;
+
+    private String majorType;
 }
