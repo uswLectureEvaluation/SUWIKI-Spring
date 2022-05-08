a9371757b2e7492b6299f69a74ba4404c89af9cf
Diger-king <peter6081@gmail.com>
2022-05-08 23:37:02 +0900

    init Test

--- src/test/java/Suwikibackend/Suwiki/Json/JsonTest.java
+++ src/test/java/Suwikibackend/Suwiki/Json/JsonTest.java
@@ -1,29 +1,29 @@
-package Suwikibackend.Suwiki.Json;
-
-import org.json.simple.parser.ParseException;
-import org.springframework.transaction.annotation.Transactional;
-import usw.suwiki.SuwikiApplication;
-import usw.suwiki.domain.lecture.Lecture;
-import usw.suwiki.repository.evaluation.JpaEvaluatePostsRepository;
-import usw.suwiki.service.util.JsonToDataTable;
-import usw.suwiki.repository.lecture.JpaLectureRepository;
-import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import usw.suwiki.service.util.JsonToDataTable;
-
-import java.io.IOException;
-
-//@Transactional
-@SpringBootTest(classes = SuwikiApplication.class)
-public class JsonTest {
-
-    @Autowired
-    JsonToDataTable jsonToDataTable;
-
-    @Test
-    public void registration_json() throws IOException, ParseException, InterruptedException {
-        jsonToDataTable.toEntity();
-    }
-}
+//package Suwikibackend.Suwiki.Json;
+//
+//import org.json.simple.parser.ParseException;
+//import org.springframework.transaction.annotation.Transactional;
+//import usw.suwiki.SuwikiApplication;
+//import usw.suwiki.domain.lecture.Lecture;
+//import usw.suwiki.repository.evaluation.JpaEvaluatePostsRepository;
+//import usw.suwiki.service.util.JsonToDataTable;
+//import usw.suwiki.repository.lecture.JpaLectureRepository;
+//import org.json.simple.parser.ParseException;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import usw.suwiki.service.util.JsonToDataTable;
+//
+//import java.io.IOException;
+//
+////@Transactional
+//@SpringBootTest(classes = SuwikiApplication.class)
+//public class JsonTest {
+//
+//    @Autowired
+//    JsonToDataTable jsonToDataTable;
+//
+//    @Test
+//    public void registration_json() throws IOException, ParseException, InterruptedException {
+//        jsonToDataTable.toEntity();
+//    }
+//}
--- src/test/java/Suwikibackend/Suwiki/repository/LectureRepositoryTest.java
+++ src/test/java/Suwikibackend/Suwiki/repository/LectureRepositoryTest.java
@@ -1,68 +1,68 @@
-package Suwikibackend.Suwiki.repository;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import usw.suwiki.SuwikiApplication;
-import usw.suwiki.domain.lecture.Lecture;
-import usw.suwiki.dto.lecture.LectureFindOption;
-import usw.suwiki.dto.lecture.LectureListAndCountDto;
-import usw.suwiki.repository.evaluation.JpaEvaluatePostsRepository;
-import usw.suwiki.repository.exam.JpaExamPostsRepository;
-import usw.suwiki.repository.lecture.JpaLectureRepository;
-import usw.suwiki.service.evaluation.EvaluatePostsService;
-
-import javax.transaction.Transactional;
-import java.util.Optional;
-
-
-@Transactional
-@SpringBootTest(classes = SuwikiApplication.class)
-public class LectureRepositoryTest {
-
-    @Autowired
-    JpaLectureRepository jpaLectureRepository;
-
-    @Test
-    public void findAllLectureByMajorTypeTest() {
-        LectureFindOption option = LectureFindOption.builder().majorType(Optional.of("간호학과")).pageNumber(Optional.of(1)).orderOption(Optional.of("modifiedDate")).build();
-        LectureListAndCountDto dto = jpaLectureRepository.findAllLectureByMajorType(option);
-
-        System.out.println(dto.getCount());
-        for (Lecture lecture : dto.getLectureList()) {
-            System.out.println(lecture.getLectureName() + " ," + lecture.getMajorType());
-        }
-    }
-
-    @Test
-    public void testHandleNullProfessor() {
-        LectureFindOption option = LectureFindOption.builder().majorType(Optional.of("")).pageNumber(Optional.of(1)).orderOption(Optional.of("modifiedDate")).build();
-        LectureListAndCountDto dto = jpaLectureRepository.findLectureByFindOption("전공실기",option);
-
-        System.out.println(dto.getCount());
-        for (Lecture lecture : dto.getLectureList()) {
-            System.out.println(lecture.getLectureName() + " ," + lecture.getProfessor());
-            System.out.println(lecture.getProfessor().getClass());
-        }
-    }
-
-    @Test
-    public void nullTest() {
-        LectureFindOption option = LectureFindOption.builder().majorType(Optional.of("")).pageNumber(Optional.of(1)).orderOption(Optional.of("modifiedDate")).build();
-
-        Object object = new Object();
-        object = null;
-        String a = new String();
-        a = null;
-        String b = null;
-        String c = String.valueOf(object);
-        String e = c;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(object);
-        System.out.println(e);
-
-    }
-}
-
+//package Suwikibackend.Suwiki.repository;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import usw.suwiki.SuwikiApplication;
+//import usw.suwiki.domain.lecture.Lecture;
+//import usw.suwiki.dto.lecture.LectureFindOption;
+//import usw.suwiki.dto.lecture.LectureListAndCountDto;
+//import usw.suwiki.repository.evaluation.JpaEvaluatePostsRepository;
+//import usw.suwiki.repository.exam.JpaExamPostsRepository;
+//import usw.suwiki.repository.lecture.JpaLectureRepository;
+//import usw.suwiki.service.evaluation.EvaluatePostsService;
+//
+//import javax.transaction.Transactional;
+//import java.util.Optional;
+//
+//
+//@Transactional
+//@SpringBootTest(classes = SuwikiApplication.class)
+//public class LectureRepositoryTest {
+//
+//    @Autowired
+//    JpaLectureRepository jpaLectureRepository;
+//
+//    @Test
+//    public void findAllLectureByMajorTypeTest() {
+//        LectureFindOption option = LectureFindOption.builder().majorType(Optional.of("간호학과")).pageNumber(Optional.of(1)).orderOption(Optional.of("modifiedDate")).build();
+//        LectureListAndCountDto dto = jpaLectureRepository.findAllLectureByMajorType(option);
+//
+//        System.out.println(dto.getCount());
+//        for (Lecture lecture : dto.getLectureList()) {
+//            System.out.println(lecture.getLectureName() + " ," + lecture.getMajorType());
+//        }
+//    }
+//
+//    @Test
+//    public void testHandleNullProfessor() {
+//        LectureFindOption option = LectureFindOption.builder().majorType(Optional.of("")).pageNumber(Optional.of(1)).orderOption(Optional.of("modifiedDate")).build();
+//        LectureListAndCountDto dto = jpaLectureRepository.findLectureByFindOption("전공실기",option);
+//
+//        System.out.println(dto.getCount());
+//        for (Lecture lecture : dto.getLectureList()) {
+//            System.out.println(lecture.getLectureName() + " ," + lecture.getProfessor());
+//            System.out.println(lecture.getProfessor().getClass());
+//        }
+//    }
+//
+//    @Test
+//    public void nullTest() {
+//        LectureFindOption option = LectureFindOption.builder().majorType(Optional.of("")).pageNumber(Optional.of(1)).orderOption(Optional.of("modifiedDate")).build();
+//
+//        Object object = new Object();
+//        object = null;
+//        String a = new String();
+//        a = null;
+//        String b = null;
+//        String c = String.valueOf(object);
+//        String e = c;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(object);
+//        System.out.println(e);
+//
+//    }
+//}
+//
