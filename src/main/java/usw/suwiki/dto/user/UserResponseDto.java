b64e8a2ad133af404ede8ad8bee6e9a49acf12bd
Diger-king <peter6081@gmail.com>
2022-05-12 23:18:08 +0900

    Fix : Dto - UserResponseDto MyPageResponse 수정

--- src/main/java/usw/suwiki/dto/user/UserResponseDto.java
+++ src/main/java/usw/suwiki/dto/user/UserResponseDto.java
@@ -14,7 +14,7 @@ public class UserResponseDto {
         private String loginId; // 유저 로그인 아이디
         private String email;
         private Integer point; //유저 포인트
-        private Integer writtenLecture; //유저 작성한 강의평가 갯수
+        private Integer writtenEvaluation; //유저 작성한 강의평가 갯수
         private Integer writtenExam; //유저 작성한 시험정보 갯수
         private Integer viewExam; //조회환 시험정보 갯수
     }
