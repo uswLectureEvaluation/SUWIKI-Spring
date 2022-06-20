47539912c220bb9b985d606e2f6e7209198d5946
Diger-king <peter6081@gmail.com>
2022-06-21 00:22:22 +0900

    RollBack - AdminController

--- src/main/java/usw/suwiki/domain/userAdmin/UserAdminController.java
+++ src/main/java/usw/suwiki/domain/userAdmin/UserAdminController.java
@@ -19,11 +19,11 @@ import usw.suwiki.domain.user.UserService;
 
 import javax.validation.Valid;
 import java.util.HashMap;
-import java.util.Optional;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/proxy/admin")
+@RequestMapping("/admin")
 public class UserAdminController {
 
     // JWT 관련 의존성
