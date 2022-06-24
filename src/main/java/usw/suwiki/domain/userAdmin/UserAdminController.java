64b98deb7cd7e0c95ff41f89fdafe6dd8c798b66
Diger-king <peter6081@gmail.com>
2022-06-25 00:33:34 +0900

    Feat : CORS Adding @CrossOrigin

--- src/main/java/usw/suwiki/domain/userAdmin/UserAdminController.java
+++ src/main/java/usw/suwiki/domain/userAdmin/UserAdminController.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserAdminController {
 
     // JWT 관련 의존성
