3b8e3550b8825c80f58381764f8465ba5b0d4f09
Diger-king <peter6081@gmail.com>
2022-06-25 16:40:46 +0900

    Fix : Report Repository DeleteByXX Adjust @Transactional

--- src/main/java/usw/suwiki/domain/reportTarget/EvaluateReportRepository.java
+++ src/main/java/usw/suwiki/domain/reportTarget/EvaluateReportRepository.java
@@ -3,15 +3,18 @@ package usw.suwiki.domain.reportTarget;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface EvaluateReportRepository extends JpaRepository<EvaluatePostReport, Long> {
 
     @Query(value = "SELECT * FROM evaluate_post_report", nativeQuery = true)
     List<EvaluatePostReport> loadAllReportedPosts();
 
+    @Transactional
     void deleteByEvaluateIdx(Long evaluateIdx);
 
 }
