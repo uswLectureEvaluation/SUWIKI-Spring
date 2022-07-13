cd2520a90ac301aa3f47537d480f1e20ab1d8e10
Diger-king <peter6081@gmail.com>
2022-07-14 01:34:32 +0900

    Fix : 주석해제

--- src/test/java/Suwikibackend/LectureDataTestPackage/SuwikiApplicationTests.java
+++ src/test/java/Suwikibackend/LectureDataTestPackage/SuwikiApplicationTests.java
@@ -1,6 +1,5 @@
-package Suwikibackend.Suwiki;
+package Suwikibackend.LectureDataTestPackage;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
