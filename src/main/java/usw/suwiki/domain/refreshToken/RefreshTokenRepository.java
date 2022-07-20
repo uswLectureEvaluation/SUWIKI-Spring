f9ddb620daaa459d41dab40eba12b9770724592c
Diger-king <peter6081@gmail.com>
2022-07-20 20:18:56 +0900

    Fix : RefreshToken Query

--- src/main/java/usw/suwiki/domain/refreshToken/RefreshTokenRepository.java
+++ src/main/java/usw/suwiki/domain/refreshToken/RefreshTokenRepository.java
@@ -13,7 +13,8 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByPayload(String payload);
 
-    Optional<RefreshToken> findByUser(Long id);
+    @Query(value = "SELECT * FROM RefreshToken WHERE user_idx = :id", nativeQuery = true)
+    Optional<RefreshToken> findByUser(@Param("id") Long id);
 
     //페이로드 업데이트
     @Modifying(clearAutomatically = true)
