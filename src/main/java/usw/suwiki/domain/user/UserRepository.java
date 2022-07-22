37804fd0b96a7e115f1afcb5019f35eee7387da0
Diger-king <peter6081@gmail.com>
2022-07-22 19:58:14 +0900

    오타 수정

--- src/main/java/usw/suwiki/domain/user/UserRepository.java
+++ src/main/java/usw/suwiki/domain/user/UserRepository.java
@@ -51,7 +51,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "password = (SELECT password FROM user_isolation WHERE user_idx = :id)," +
             "email = (SELECT email FROM user_isolation WHERE user_idx = :id)," +
             "last_login = (SELECT last_login FROM user_isolation WHERE user_idx = :id)," +
-            "requested_quit_date = (SELECT requested_date FROM user_isolation WHERE user_idx = :id)", nativeQuery = true)
+            "requested_quit_date = (SELECT requested_quit_date FROM user_isolation WHERE user_idx = :id)", nativeQuery = true)
     void convertToWakeUp(@Param("id") Long id);
 
 
