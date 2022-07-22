5be7ad9f2addf2bb7208d15cf4b7cd57c9cbe939
Diger-king <peter6081@gmail.com>
2022-07-22 19:19:38 +0900

    fix : UserSionation Column

--- src/main/java/usw/suwiki/domain/userIsolation/UserIsolation.java
+++ src/main/java/usw/suwiki/domain/userIsolation/UserIsolation.java
@@ -27,37 +27,4 @@ public class UserIsolation {
 
     @Column
     private String email; //Email
-
-    @Column
-    private boolean restricted; //이메일 인증 유무 + 제재유저 대상
-
-    @Column
-    private Integer restrictedCount; //정지횟수
-
-    @Enumerated(EnumType.STRING)
-    private Role role; //User Auth
-
-    @Column
-    private Integer writtenEvaluation;
-
-    @Column
-    private Integer writtenExam;
-
-    @Column
-    private Integer viewExamCount;
-
-    @Column
-    private Integer point;
-
-    @Column
-    private LocalDateTime lastLogin; //Last Login Time
-
-    @Column
-    private LocalDateTime requestedQuitDate; //회원탈퇴 요청 일시
-
-    @Column
-    private LocalDateTime createdAt; //Join Time
-
-    @Column
-    private LocalDateTime updatedAt;
 }
