5a01f50f91117de37d8b1f823edd4bbea199361a
JIWEON-JEONG <dlektl6@naver.com>
2022-07-29 00:22:57 +0900

    [REFACTOR] effective java item4 적용 with 도현

--- src/main/java/usw/suwiki/SuwikiVersion.java
+++ src/main/java/usw/suwiki/SuwikiVersion.java
@@ -1,8 +1,16 @@
 package usw.suwiki;
 
+import jdk.jshell.execution.Util;
 import lombok.Getter;
 
+/**
+ * effective java item4 적용.
+ */
 @Getter
 public class SuwikiVersion {
     public static final float version = 1;
+
+    //never instantiated
+    private SuwikiVersion() {
+    }
 }
