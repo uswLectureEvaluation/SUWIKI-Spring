f3678b22b9a9832ada40740318980ecba08eae2b
K-Diger <tigerkingdh991009@gmail.com>
2022-08-29 22:05:16 +0900

    test

--- src/main/java/usw/suwiki/domain/userAdmin/UserAdminController.java
+++ src/main/java/usw/suwiki/domain/userAdmin/UserAdminController.java
@@ -254,7 +254,6 @@ public class UserAdminController {
         return result;
     }
 
-
     // 신고받은 게시글 리스트 불러오기
     @GetMapping("/report/list")
     public ResponseEntity<UserAdminResponseDto.ViewAllReportedPost> loadReportedPost(@Valid @RequestHeader String Authorization) {
