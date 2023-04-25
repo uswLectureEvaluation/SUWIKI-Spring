97b317b44ee1606302b9cd2308b04411fcf1c068
K-Diger <tigerkingdh991009@gmail.com>
2023-04-26 00:11:06 +0900

    chore : JWT 만료기간 변경

--- src/main/java/usw/suwiki/global/jwt/JwtProvider.java
+++ src/main/java/usw/suwiki/global/jwt/JwtProvider.java
@@ -21,9 +21,9 @@ public class JwtProvider {
     @Value("${spring.jwt.secret-key}")
     private String secretKey;
 
-        private static final Long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L; // 30분
+    //        private static final Long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L; // 30분
     private static final Long REFRESH_TOKEN_EXPIRE_TIME = 270 * 24 * 60 * 60 * 1000L; // 270일 -> 9개월
-//    private static final Long ACCESS_TOKEN_EXPIRE_TIME = 1 * 1000L; // 1초
+    private static final Long ACCESS_TOKEN_EXPIRE_TIME = 1 * 1000L; // 1초
 //    private static final Long REFRESH_TOKEN_EXPIRE_TIME = 5 * 60 * 1000L; // 5분
 
     private final RefreshTokenRepository refreshTokenRepository;
