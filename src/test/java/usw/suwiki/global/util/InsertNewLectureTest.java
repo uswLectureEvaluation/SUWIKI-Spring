eb5f053c026a04d92c72f79859ee0eaa9d2f89ea
JIWEON-JEONG <dlektl6@naver.com>
2023-05-01 15:13:26 +0900

    [#16] feature : 강의데이터 추가 & 버전 업데이트

---
+++ src/test/java/usw/suwiki/global/util/InsertNewLectureTest.java
@@ -0,0 +1,29 @@
+package usw.suwiki.global.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
+
+@ActiveProfiles("dev")
+@SpringBootTest
+class InsertNewLectureTest {
+
+	@Autowired
+	JsonToDataTable jsonToDataTable;
+
+	@Autowired
+	LectureRepository lectureRepository;
+
+	@Test
+	void insertNewLecture() throws IOException, ParseException, InterruptedException {
+		jsonToDataTable.toEntity("src/main/resources/USW_2023_1.json");
+	}
+}
