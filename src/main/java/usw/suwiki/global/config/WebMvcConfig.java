918aa4f5feacb42116015f784b4bbdf76540034d
K-Diger <tigerkingdh991009@gmail.com>
2023-05-14 14:13:25 +0900

    fix : 테스트코드 실행 시 swagger와 관련된 에러가 발생하던 문제 해결

--- src/main/java/usw/suwiki/global/config/WebMvcConfig.java
+++ src/main/java/usw/suwiki/global/config/WebMvcConfig.java
@@ -3,17 +3,19 @@ package usw.suwiki.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import usw.suwiki.global.interceptor.JwtInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     private final JwtInterceptor jwtInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor);
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/swagger-ui.html/**"); // 스웨거 UI 예외 처리
     }
 }
--- src/main/java/usw/suwiki/global/swagger/SwaggerConfiguration.java
+++ src/main/java/usw/suwiki/global/swagger/SwaggerConfiguration.java
@@ -8,6 +8,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import usw.suwiki.global.interceptor.JwtInterceptor;
 
 @Configuration
 public class SwaggerConfiguration {
