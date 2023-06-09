1cb6a95557272454d1857897c93410013076bdfa
K-Diger <tigerkingdh991009@gmail.com>
2023-06-10 00:09:52 +0900

    docs : Swagger Title 및 Description 변경

--- src/main/java/usw/suwiki/global/swagger/SwaggerConfiguration.java
+++ src/main/java/usw/suwiki/global/swagger/SwaggerConfiguration.java
@@ -1,35 +1,21 @@
 package usw.suwiki.global.swagger;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
     @Bean
     public OpenAPI openApi() {
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("AccessToken");
 
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
                 .info(new Info()
-                        .title("Ratrip API")
-                        .description("[인증]이 붙은 API는 요청 헤더에 Key: Authorization, Value: {token}을 포함해야 합니다."))
-                .components(new Components()
-                        .addSecuritySchemes("Access Token",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                        .in(SecurityScheme.In.HEADER)
-                                        .name("Authorization")))
-                .security(List.of(securityRequirement));
+                        .title("SUWIKI API")
+                        .description("Description By Richard With Diger"));
     }
 
 }
