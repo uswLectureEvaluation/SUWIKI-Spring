9d47b71dc764a2c35f7047b69bdf3276c2160621
Diger <kitten.diger@gmail.com>
2023-07-30 23:10:31 +0900

    reinit by gitignore

--- src/test/java/usw/suwiki/global/util/InsertNewLectureTest.java
+++ src/test/java/usw/suwiki/global/util/InsertNewLectureTest.java
@@ -1,31 +1,25 @@
 package usw.suwiki.global.util;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.IOException;
-
-import javax.persistence.EntityManager;
-
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
 import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
 
-@ActiveProfiles("test")
+import java.io.IOException;
+
+// @ActiveProfiles("test")
 @SpringBootTest
 class InsertNewLectureTest {
 
-	@Autowired
-	JsonToDataTable jsonToDataTable;
+    @Autowired
+    JsonToDataTable jsonToDataTable;
 
-	@Autowired
-	LectureRepository lectureRepository;
+    @Autowired
+    LectureRepository lectureRepository;
 
-	@Test
-	void insertNewLecture() throws IOException, ParseException, InterruptedException {
-		jsonToDataTable.toEntity("src/main/resources/USW_2023_1.json");
-	}
+    @Test
+    void insertNewLecture() throws IOException, ParseException, InterruptedException {
+        jsonToDataTable.toEntity("src/main/resources/USW_2023_2.json");
+    }
 }
