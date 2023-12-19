19197f0e9272c987b48a76a483bec694ae25a06e
WooYoungDoo <duden5000@naver.com>
2023-12-19 21:02:09 +0900

    feat: favoriteMajor의 예외 처리 클래스

---
+++ src/main/java/usw/suwiki/global/exception/errortype/FavoriteMajorException.java
@@ -0,0 +1,9 @@
+package usw.suwiki.global.exception.errortype;
+
+import usw.suwiki.global.exception.ExceptionType;
+
+public class FavoriteMajorException extends BaseException{
+    public FavoriteMajorException(ExceptionType exceptionType) {
+        super(exceptionType);
+    }
+}
