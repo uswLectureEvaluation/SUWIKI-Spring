b3347e67f5576eecc03b1052b9d4d1e7cc24ac81
WooYoungDoo <duden5000@naver.com>
2023-12-19 21:03:41 +0900

    refactor: Jpql -> Spring Data JPA 변화에 따른 코드 변화

--- src/main/java/usw/suwiki/domain/favoritemajor/service/FavoriteMajorService.java
+++ src/main/java/usw/suwiki/domain/favoritemajor/service/FavoriteMajorService.java
@@ -6,41 +6,62 @@ import usw.suwiki.domain.favoritemajor.FavoriteMajor;
 import usw.suwiki.domain.favoritemajor.dto.FavoriteSaveDto;
 import usw.suwiki.domain.favoritemajor.repository.FavoriteMajorRepository;
 import usw.suwiki.domain.user.user.User;
-import usw.suwiki.domain.user.user.repository.UserRepository;
+import usw.suwiki.domain.user.user.service.UserCRUDService;
+import usw.suwiki.global.exception.errortype.FavoriteMajorException;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
+
+import static usw.suwiki.global.exception.ExceptionType.*;
 
 @Transactional
 @RequiredArgsConstructor
 @Service
 public class FavoriteMajorService {
+    private final UserCRUDService userCRUDService;
     private final FavoriteMajorRepository favoriteMajorRepository;
-    private final UserRepository userRepository;
 
-    public void save(FavoriteSaveDto dto, Long userIdx) {
-        FavoriteMajor favorite = new FavoriteMajor(dto.getMajorType());
-        Optional<User> user = userRepository.findById(userIdx);
-        favorite.setUser(user.get());
-        favoriteMajorRepository.save(favorite);
+    /**
+     * userBusinessService 거쳐서 반환값 넘어 가는 중..
+     * userBusinessService 가 너무 커서 분리 작업할 때 이리로 코드 옮겨도 괜찮을 듯
+     */
+    public void save(FavoriteSaveDto favoriteSaveDto, Long userIdx) {
+        User loginUser = userCRUDService.loadUserByIdx(userIdx);
+        String majorType = favoriteSaveDto.getMajorType();
+        validateDuplicateFavoriteMajor(loginUser, majorType);
+        FavoriteMajor favoriteMajor = FavoriteMajor.builder()
+                .user(loginUser)
+                .majorType(majorType)
+                .build();
+        favoriteMajorRepository.save(favoriteMajor);
     }
 
-    public List<String> findMajorTypeByUser(Long userIdx) {
-        List<String> list = favoriteMajorRepository.findOnlyMajorTypeByUser(userIdx);
-        return list;
+    private void validateDuplicateFavoriteMajor(User loginUser, String majorType) {
+        boolean exists = favoriteMajorRepository.existsByUserIdAndMajorType(loginUser.getId(), majorType);
+        if (!exists) return;
+
+        throw new FavoriteMajorException(FAVORITE_MAJOR_DUPLICATE_REQUEST);
+    }
+
+    public List<String> findAllMajorTypeByUser(Long userIdx) {
+        User loginUser = userCRUDService.loadUserByIdx(userIdx);
+
+        List<FavoriteMajor> favoriteMajors = favoriteMajorRepository.findAllByUserId(loginUser.getId());
+        return favoriteMajors.stream().map(FavoriteMajor::getMajorType).toList();
     }
 
     public void delete(Long userIdx, String majorType) {
-        FavoriteMajor favorite = favoriteMajorRepository.findByUserAndMajorType(userIdx, majorType);
-        favoriteMajorRepository.delete(favorite);
+        User loginUser = userCRUDService.loadUserByIdx(userIdx);
+
+        FavoriteMajor favoriteMajor = favoriteMajorRepository.findByUserIdAndMajorType(loginUser.getId(), majorType)
+                .orElseThrow(() -> new FavoriteMajorException(FAVORITE_MAJOR_NOT_FOUND));
+
+        favoriteMajorRepository.delete(favoriteMajor);
     }
 
-    public void deleteFromUserIdx(Long userIdx) {
-        List<FavoriteMajor> list = favoriteMajorRepository.findAllByUser(userIdx);
-        for (FavoriteMajor favorite : list) {
-            favoriteMajorRepository.delete(favorite);
-        }
+    public void deleteAllFromUserIdx(Long userIdx) {
+        User loginUser = userCRUDService.loadUserByIdx(userIdx);
+        List<FavoriteMajor> favoriteMajors = favoriteMajorRepository.findAllByUserId(loginUser.getId());
+        favoriteMajorRepository.deleteAll(favoriteMajors);
     }
-
 }
