a1ecf5dba11c790d4038da842da3bcfe2155b53d
wonseok2877 <wonseok2877@gmail.com>
2023-12-26 00:39:09 +0900

    test: TimetableRepository find -> findAll

--- src/main/java/usw/suwiki/domain/timetable/repository/TimetableRepository.java
+++ src/main/java/usw/suwiki/domain/timetable/repository/TimetableRepository.java
@@ -1,10 +1,10 @@
 package usw.suwiki.domain.timetable.repository;
 
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import usw.suwiki.domain.timetable.entity.Timetable;
 
 public interface TimetableRepository extends JpaRepository<Timetable, Long> {
-    Optional<Timetable> findByUserId(Long userId);
+    List<Timetable> findAllByUserId(Long userId);
 
 }
--- src/test/java/usw/suwiki/repository/timetable/TimetableRepositoryTest.java
+++ src/test/java/usw/suwiki/repository/timetable/TimetableRepositoryTest.java
@@ -1,9 +1,9 @@
 package usw.suwiki.repository.timetable;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -21,8 +21,6 @@ import org.springframework.context.annotation.Import;
 import usw.suwiki.config.TestJpaConfig;
 import usw.suwiki.domain.timetable.entity.Semester;
 import usw.suwiki.domain.timetable.entity.Timetable;
-import usw.suwiki.domain.timetable.entity.TimetableCell;
-import usw.suwiki.domain.timetable.entity.TimetableCellColor;
 import usw.suwiki.domain.timetable.repository.TimetableRepository;
 import usw.suwiki.domain.user.user.User;
 import usw.suwiki.domain.user.user.repository.UserRepository;
@@ -50,13 +48,24 @@ public class TimetableRepositoryTest {
     void setUp() {
         this.dummyUser = userRepository.save(UserTemplate.createDummyUser());
 
-        Timetable timetable = Timetable.builder()
-                .name("내 시간표")
-                .year(2023)
-                .semester(Semester.SECOND)
-                .build();
-        timetable.associateUser(dummyUser);
+        Timetable timetable = createUserAssociatedDummyTimetable("내 시간표", 2023, Semester.SECOND, dummyUser);
         this.dummyTimetable = timetableRepository.save(timetable);
+
+        createUserAssociatedDummyTimetable("1-1 시간표", 2017, Semester.FIRST, dummyUser);
+        createUserAssociatedDummyTimetable("1-2 시간표", 2017, Semester.SECOND, dummyUser);
+        createUserAssociatedDummyTimetable("2-1 시간표", 2018, Semester.FIRST, dummyUser);
+
+        userRepository.save(dummyUser);
+    }
+
+    private Timetable createUserAssociatedDummyTimetable(String name, Integer year, Semester semester, User user) {
+        Timetable timetable = Timetable.builder()
+                .name(name)
+                .year(year)
+                .semester(semester)
+                .build();
+        timetable.associateUser(user);
+        return timetable;
     }
 
     @Test
@@ -132,42 +141,18 @@ public class TimetableRepositoryTest {
     }
 
     @Test
-    @DisplayName("SELECT Timetable userId 조회 성공")
-    public void selectTimetableByUserId_success() {
+    @DisplayName("SELECT ALL Timetable userId 조회 성공")
+    public void selectAllTimetableByUserId_success() {
         // given
         Long userId = dummyUser.getId();
 
         // when
-        Optional<Timetable> optionalTimetable = timetableRepository.findByUserId(userId);
+        List<Timetable> all = timetableRepository.findAllByUserId(userId);
 
         // then
-        assertThat(optionalTimetable.isPresent()).isTrue();
-        assertThat(optionalTimetable.get()).isEqualTo(dummyTimetable);
+        assertThat(all.isEmpty()).isFalse();
+        assertThat(all.size()).isEqualTo(4);
     }
 
-
-    // TimetableCell 테스트
-    @Test
-    @DisplayName("INSERT TimetableCell 성공 - Timetable 연관관계 편의 메서드")
-    public void insertTimetableCell_success() {
-        // given
-        TimetableCell timetableCell = TimetableCell.builder()
-                .lectureName("ICT 개론")
-                .professorName("신호진")
-                .color(TimetableCellColor.BROWN)
-                .build();
-
-        // when
-        timetableCell.associateTimetable(dummyTimetable);   // 연관관계 편의 메서드
-        entityManager.persist(dummyTimetable);
-        entityManager.flush();
-        entityManager.clear();
-
-        Timetable foundTable = entityManager.find(Timetable.class, dummyTimetable.getId());
-        TimetableCell foundCell = entityManager.find(TimetableCell.class, timetableCell.getId());
-
-        // then
-        assertThat(foundCell.getId()).isEqualTo(timetableCell.getId());
-        assertThat(foundCell.getTimetable()).isEqualTo(foundTable);
-    }
+    // TODO: 연관관계 메서드를 이용한 삭제 구현 고민
 }
