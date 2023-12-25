61081bf7a27ebd5cb7b2f28ec27335817d72cef2
wonseok2877 <wonseok2877@gmail.com>
2023-12-26 00:17:01 +0900

    feat: TimetableCellRepository 추가

---
+++ src/main/java/usw/suwiki/domain/timetable/repository/TimetableCellRepository.java
@@ -0,0 +1,9 @@
+package usw.suwiki.domain.timetable.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import usw.suwiki.domain.timetable.entity.TimetableCell;
+
+public interface TimetableCellRepository extends JpaRepository<TimetableCell, Long> {
+    List<TimetableCell> findAllByTimetableId(Long timetableId);
+}
