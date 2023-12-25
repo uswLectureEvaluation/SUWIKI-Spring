c8b4bb3163b6ba8c9f19c9e42ae0f118d6029ba0
wonseok2877 <wonseok2877@gmail.com>
2023-12-25 14:53:00 +0900

    feat: TimetableCell 엔티티 및 연관관계 메서드 추가

--- src/main/java/usw/suwiki/domain/timetable/entity/TimetableCell.java
+++ src/main/java/usw/suwiki/domain/timetable/entity/TimetableCell.java
@@ -1,8 +1,5 @@
 package usw.suwiki.domain.timetable.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -13,14 +10,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import usw.suwiki.domain.user.user.User;
 import usw.suwiki.global.BaseTimeEntity;
 
 @Entity
@@ -28,40 +23,32 @@ import usw.suwiki.global.BaseTimeEntity;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Timetable extends BaseTimeEntity {
+public class TimetableCell extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "timetable_id")
+    @Column(name = "timetable_cell_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @NotNull
+    @JoinColumn(name = "timetable_id")
+    private Timetable timetable;
 
     @NotNull
-    private String name;
+    private String lectureName;
 
     @NotNull
-    private Integer year;
+    private String professorName;
 
     @Enumerated(EnumType.STRING)
     @NotNull
-    private Semester semester;
-
-    @OneToMany(mappedBy = "timetable", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private List<TimetableCell> cellList = new ArrayList<>();
-
+    private TimetableCellColor color;
 
     // 연관관계 메서드
-    public void addCell(TimetableCell cell) {
-        this.cellList.add(cell);
-    }
-
-    public void removeCell(TimetableCell cell) {
-        this.cellList.remove(cell);
-    }
+    // TimetableElement 추가
+    // TimetableElement 삭제
 
     // 비즈니스 메서드
+    //
 
 }
---
+++ src/main/java/usw/suwiki/domain/timetable/entity/TimetableCellColor.java
@@ -0,0 +1,15 @@
+package usw.suwiki.domain.timetable.entity;
+
+public enum TimetableCellColor {
+    ORANGE, APRICOT, PINK, SKY,
+    BROWN, LIGHT_BROWN, BROWN_DARK,
+    PURPLE, PURPLE_LIGHT,
+    RED_LIGHT,
+    GREEN, GREEN_LIGHT, GREEN_DARK,
+    NAVY, NAVY_LIGHT, NAVY_DARK,
+    VIOLET, VIOLET_LIGHT,
+    GRAY, GRAY_DARK
+    ;
+
+    // String to Enum
+}
