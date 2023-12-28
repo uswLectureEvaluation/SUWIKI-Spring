a18d7b5e1e56c187820fd9fbbf839f2953bc03ef
wonseok2877 <wonseok2877@gmail.com>
2023-12-28 14:52:53 +0900

    refactor: TimetableCellColor 변환 메서드

--- src/main/java/usw/suwiki/domain/timetable/entity/TimetableCellColor.java
+++ src/main/java/usw/suwiki/domain/timetable/entity/TimetableCellColor.java
@@ -1,15 +1,39 @@
 package usw.suwiki.domain.timetable.entity;
 
-public enum TimetableCellColor {
-    ORANGE, APRICOT, PINK, SKY,
-    BROWN, LIGHT_BROWN, BROWN_DARK,
-    PURPLE, PURPLE_LIGHT,
-    RED_LIGHT,
-    GREEN, GREEN_LIGHT, GREEN_DARK,
-    NAVY, NAVY_LIGHT, NAVY_DARK,
-    VIOLET, VIOLET_LIGHT,
-    GRAY, GRAY_DARK
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import usw.suwiki.global.exception.ExceptionType;
+import usw.suwiki.global.exception.errortype.TimetableException;
+import usw.suwiki.global.util.enums.KeyValueEnumModel;
+
+@RequiredArgsConstructor
+public enum TimetableCellColor implements KeyValueEnumModel<String> {
+    ORANGE("ORANGE"), APRICOT("APRICOT"), PINK("PINK"), SKY("SKY"),
+    BROWN("BROWN"), LIGHT_BROWN("LIGHT_BROWN"), BROWN_DARK("BROWN_DARK"),
+    PURPLE("PURPLE"), PURPLE_LIGHT("PURPLE_LIGHT"),
+    RED_LIGHT("RED_LIGHT"),
+    GREEN("GREEN"), GREEN_LIGHT("GREEN_LIGHT"), GREEN_DARK("GREEN_DARK"),
+    NAVY("NAVY"), NAVY_LIGHT("NAVY_LIGHT"), NAVY_DARK("NAVY_DARK"),
+    VIOLET("VIOLET"), VIOLET_LIGHT("VIOLET_LIGHT"),
+    GRAY("GRAY"), GRAY_DARK("GRAY_DARK")
     ;
 
-    // String to Enum
+    private final String value;
+
+    public static TimetableCellColor ofString(String param) {
+        return Arrays.stream(TimetableCellColor.values())
+                .filter(v -> v.getValue().equals(param.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> new TimetableException(ExceptionType.INVALID_TIMETABLE_CELL_COLOR));
+    }
+
+    @Override
+    public String getKey() {
+        return this.name();
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
 }
