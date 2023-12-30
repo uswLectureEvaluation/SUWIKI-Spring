53673073fda21248ac3ada0c36e18bb11be85456
wonseok2877 <wonseok2877@gmail.com>
2023-12-30 22:30:18 +0900

    refactor: TimetableDay 이넘 모델 상속

--- src/main/java/usw/suwiki/domain/timetable/entity/TimetableDay.java
+++ src/main/java/usw/suwiki/domain/timetable/entity/TimetableDay.java
@@ -1,8 +1,32 @@
 package usw.suwiki.domain.timetable.entity;
 
-public enum TimetableDay {
-    MON, TUE, WED, THU, FRI, SAT, E_LEARNING
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import usw.suwiki.global.exception.ExceptionType;
+import usw.suwiki.global.exception.errortype.TimetableException;
+import usw.suwiki.global.util.enums.KeyValueEnumModel;
+
+@RequiredArgsConstructor
+public enum TimetableDay implements KeyValueEnumModel<String> {
+    MON("MON"), TUE("TUE"), WED("WED"), THU("THU"), FRI("FRI"), SAT("SAT"), E_LEARNING("E_LEARNING")
     ;
 
-    // String to Enum
+    private final String value;
+
+    public static TimetableDay ofString(String param) {
+        return Arrays.stream(TimetableDay.values())
+                .filter(v -> v.getValue().equals(param.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> new TimetableException(ExceptionType.INVALID_TIMETABLE_CELL_DAY));
+    }
+
+    @Override
+    public String getKey() {
+        return this.name();
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
 }
