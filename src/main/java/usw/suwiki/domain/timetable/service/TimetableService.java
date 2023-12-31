525a8db0e8ae1b6fab9d302416a7e3616a9c27df
wonseok2877 <wonseok2877@gmail.com>
2023-12-31 21:47:03 +0900

    feat: Timetable Service 시간표 셀 생성

--- src/main/java/usw/suwiki/domain/timetable/service/TimetableService.java
+++ src/main/java/usw/suwiki/domain/timetable/service/TimetableService.java
@@ -5,12 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import usw.suwiki.domain.timetable.dto.request.CreateTimetableCellRequest;
 import usw.suwiki.domain.timetable.dto.request.CreateTimetableRequest;
 import usw.suwiki.domain.timetable.dto.request.UpdateTimetableRequest;
 import usw.suwiki.domain.timetable.dto.response.SimpleTimetableResponse;
+import usw.suwiki.domain.timetable.dto.response.TimetableCellResponse;
 import usw.suwiki.domain.timetable.dto.response.TimetableResponse;
 import usw.suwiki.domain.timetable.entity.Semester;
 import usw.suwiki.domain.timetable.entity.Timetable;
+import usw.suwiki.domain.timetable.entity.TimetableCell;
+import usw.suwiki.domain.timetable.entity.TimetableDay;
 import usw.suwiki.domain.timetable.repository.TimetableCellRepository;
 import usw.suwiki.domain.timetable.repository.TimetableRepository;
 import usw.suwiki.domain.user.user.User;
@@ -41,10 +45,7 @@ public class TimetableService {
     // 시간표 수정
     @Transactional
     public SimpleTimetableResponse updateTimetable(UpdateTimetableRequest request, Long timetableId, Long userId) {
-        User user = userCRUDService.loadUserById(userId);
-        Timetable timetable = timetableRepository.findById(timetableId)
-                .orElseThrow(() -> new TimetableException(ExceptionType.TIMETABLE_NOT_FOUND));
-        timetable.validateIsAuthor(user);
+        Timetable timetable = resolveExactAuthorTimetable(timetableId, userId);
 
         timetable.update(request.getName(), request.getYear(), Semester.ofString(request.getSemester()));
         return SimpleTimetableResponse.of(timetable);
@@ -53,10 +54,7 @@ public class TimetableService {
     // 시간표 삭제
     @Transactional
     public ResultResponse deleteTimetable(Long timetableId, Long userId) {
-        User user = userCRUDService.loadUserById(userId);
-        Timetable timetable = timetableRepository.findById(timetableId)
-                .orElseThrow(() -> new TimetableException(ExceptionType.TIMETABLE_NOT_FOUND));
-        timetable.validateIsAuthor(user);
+        Timetable timetable = resolveExactAuthorTimetable(timetableId, userId);
 
         timetableRepository.delete(timetable);
         return ResultResponse.of(true);
@@ -72,18 +70,39 @@ public class TimetableService {
     }
 
     // 시간표 상세 조회
-    public TimetableResponse getTimetable(Long timetableId) {   // TODO: 동일 유저 검증 로직 추가 고민
+    public TimetableResponse getTimetable(Long timetableId) {   // TODO: 동일 유저 검증 로직 추가
         Timetable timetable = timetableRepository.findById(timetableId)
                 .orElseThrow(() -> new TimetableException(ExceptionType.TIMETABLE_NOT_FOUND));
 
         return TimetableResponse.of(timetable);
     }
 
-    // 시간표 강의 - 생성
+    // 시간표 셀 생성
+    @Transactional
+    public TimetableCellResponse createTimetableCell(CreateTimetableCellRequest request, Long timetableId, Long userId) {
+        Timetable timetable = resolveExactAuthorTimetable(timetableId, userId);
+        timetable.validateCellDayAndPeriodsDuplication(
+                TimetableDay.ofString(request.getDay()),
+                request.getStartPeriod(),
+                request.getEndPeriod()
+        );
 
-    // 시간표 강의 - 수정
+        TimetableCell timetableCell = request.toEntity(timetable);
+        timetableCellRepository.save(timetableCell);
+        return TimetableCellResponse.of(timetableCell);
+    }
 
-    // 시간표 강의 - 삭제
+    private Timetable resolveExactAuthorTimetable(Long timetableId, Long userId) {
+        User user = userCRUDService.loadUserById(userId);
+        Timetable timetable = timetableRepository.findById(timetableId)
+                .orElseThrow(() -> new TimetableException(ExceptionType.TIMETABLE_NOT_FOUND));
+        timetable.validateIsAuthor(user);
+        return timetable;
+    }
+
+    // 시간표 셀 수정
+
+    // 시간표 셀 삭제
 
     // 시간표 일괄 DB 동기화 (시간표 및 강의 bulk 생성)
 }
