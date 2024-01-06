abf01b4f7a243581ba696add058a7aba35822e39
wonseok2877 <wonseok2877@gmail.com>
2024-01-06 20:28:43 +0900

    feat: Lecture Repository 시간표상 강의 조회

--- src/main/java/usw/suwiki/domain/lecture/domain/repository/LectureQueryRepository.java
+++ src/main/java/usw/suwiki/domain/lecture/domain/repository/LectureQueryRepository.java
@@ -1,21 +1,33 @@
 package usw.suwiki.domain.lecture.domain.repository;
 
-import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import org.springframework.data.domain.Slice;
 import usw.suwiki.domain.lecture.controller.dto.LectureFindOption;
 import usw.suwiki.domain.lecture.domain.Lecture;
 import usw.suwiki.domain.lecture.domain.repository.dao.LecturesAndCountDao;
 
-import java.util.List;
-
 
 public interface LectureQueryRepository {
+    Slice<Lecture> findCurrentSemesterLecturesPage(
+            final Long cursorId,
+            final int limit,
+            final String keyword,
+            final String majorType,
+            final Integer grade
+    );
+
     //
     Lecture verifyJsonLecture(String lectureName, String ProfessorName, String majorType);
+
     //
     List<String> findAllMajorType();
+
     //
     LecturesAndCountDao findLectureByFindOption(String searchValue, LectureFindOption lectureFindOption);
+
     LecturesAndCountDao findLectureByMajorType(String searchValue, LectureFindOption lectureFindOption);
+
     LecturesAndCountDao findAllLectureByFindOption(LectureFindOption lectureFindOption);
+
     LecturesAndCountDao findAllLectureByMajorType(LectureFindOption lectureFindOption);
 }
