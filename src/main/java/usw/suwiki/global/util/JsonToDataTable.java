fe9201add4f5854e99c43324660f6a44df8f52da
wonseok2877 <wonseok2877@gmail.com>
2024-01-18 16:56:38 +0900

    comment: TODO 정리

--- src/main/java/usw/suwiki/global/util/JsonToDataTable.java
+++ src/main/java/usw/suwiki/global/util/JsonToDataTable.java
@@ -3,6 +3,7 @@ package usw.suwiki.global.util;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
@@ -17,11 +18,13 @@ import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
 @Service
 @Transactional
 @RequiredArgsConstructor
+// TODO refactor: Json - JsonToLectureForm 변환 책임 분리
+// TODO feat: 데이터 적재 과정을 admin API로 생성
 public class JsonToDataTable {
 
     private final LectureRepository lectureRepository;
 
-    //이상한 강의명 예외 처리 로직.
+    // 이상한 강의명 분기 처리 로직.
     private JsonToLectureForm handleLectureNameException(JsonToLectureForm dto) {
 
         if (dto.getLectureName().contains("재수강-")) {
@@ -57,6 +60,9 @@ public class JsonToDataTable {
         return dto;
     }
 
+    // JSON File path -> 강의 데이터 변환
+    // TODO style: 메서드명 변경
+    // TODO refactor: try catch문. try resource문도 가능한가? File이면.
     public void toEntity(String path) throws IOException, ParseException, InterruptedException {
 
         Reader reader = new FileReader(path);
@@ -70,14 +76,21 @@ public class JsonToDataTable {
             for (int i = 0; i < jsonArray.size(); i++) {
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
 
+                // TODO sytle: dto -> vo
                 JsonToLectureForm dto = JsonToLectureForm.builder()
+                        // TODO refactor: 매직 리터럴 상수화
                         .capprType((String) jsonObject.get("capprTypeNm"))
                         .evaluateType((String) jsonObject.get("cretEvalNm"))
                         .lectureCode((String) jsonObject.get("subjtCd"))
                         .selectedSemester(jsonObject.get("subjtEstbYear") + "-" + String.valueOf(
                                 jsonObject.get("subjtEstbSmrCd")).charAt(0))
+                        // TODO fix: grade 값이 0으로 삽입되는 문제. primitive type(int) => null값일 때 0으로 들어가는 것일 수 있음
+                        // 이거 이미 JSON에서 int 아닌가?
                         .grade(Integer.parseInt(jsonObject.get("trgtGrdeCd").toString()))
                         .lectureType((String) jsonObject.get("facDvnm"))
+
+                        // TODO fix: 강의 장소-교시 컬럼 (place_schedule) 누락
+                        // TODO fix: place_schedule - 강의 장소 스트링 누락
                         .placeSchedule(String.valueOf(jsonObject.get("timtSmryCn")))
                         .diclNo(String.valueOf(jsonObject.get("diclNo")))
                         .majorType(String.valueOf(jsonObject.get("estbDpmjNm")))
@@ -87,6 +100,8 @@ public class JsonToDataTable {
                         .build();
 
                 //professor 없으면 "-" 로 채움 (null 값 들어가지 않게)
+                // TODO fix: 강의 교수명 (professor) 컬럼 누락
+                // TODO refactor: JsonToLectureForm의 책임. 이동
                 if (dto.getProfessor().isEmpty() || dto.getProfessor() == null) {
                     dto.setProfessor("-");
                 }
@@ -108,18 +123,23 @@ public class JsonToDataTable {
                     lecture.setType(dto.getLectureType());
                 }
 
-                if (lecture != null) {
+                    /**
+                     * TODO think:
+                     * Lecture은 잘 저장되지만, 많은 수의 place_schedule들이 누락되고 있다.
+                     */
                     if (!lecture.getSemester().contains(dto.getSelectedSemester())) {
                         String updateString =
                                 lecture.getSemester() + ", " + dto.getSelectedSemester();
                         lecture.setSemester(updateString);  //refactoring 필요
                         lectureRepository.save(lecture);
                     }
-                } else if (lecture == null) {
+                } else {
+                    // TODO refactor: JsonToLectureForm -> Lecture 의존하도록
                     Lecture savedLecture = Lecture.toEntity(dto);
-                    Thread.sleep(1);
+                    Thread.sleep(1);    // TODO figure: 왜 필요하지?
                     lectureRepository.save(savedLecture);
                 }
+
             }
         }
     }
