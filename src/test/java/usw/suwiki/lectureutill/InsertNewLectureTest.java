66eb02163433ba530da2dd154d836456604ce8ea
wonseok2877 <wonseok2877@gmail.com>
2024-01-23 16:46:22 +0900

    refactor: 테스트 프로파일 제거

--- src/test/java/usw/suwiki/lectureutill/InsertNewLectureTest.java
+++ src/test/java/usw/suwiki/lectureutill/InsertNewLectureTest.java
@@ -3,13 +3,10 @@ package usw.suwiki.lectureutill;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
 import usw.suwiki.domain.lecture.service.LectureService;
 
-// TODO fix: 테스트에서 제거
-// TODO: 다른 테스트 코드들에도 "test" 프로파일 설정 혹은 default 프로파일로 변경. ActiveProfile 필요성 고민
-@ActiveProfiles("test")
+// WARN: 통합 테스트에 포함되어선 안 됩니다.
 @SpringBootTest
 class InsertNewLectureTest {
 
@@ -19,6 +16,7 @@ class InsertNewLectureTest {
     @Autowired
     LectureRepository lectureRepository;
 
+    // TODO: 파일 4개 운영 DB에 반영 -> 해당 테스트 전체 주석 처리
     @Test
     void insertNewLecture() {
         lectureService.bulkSaveJsonLectures("src/main/resources/USW_2023_2.json");
