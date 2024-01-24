040912ebf01e81f1b3518e94be87046f53332c16
wonseok2877 <wonseok2877@gmail.com>
2024-01-24 13:29:43 +0900

    refactor: JPA Test 공통 어노테이션

---
+++ src/test/java/usw/suwiki/global/annotation/SuwikiJpaTest.java
@@ -0,0 +1,23 @@
+package usw.suwiki.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import usw.suwiki.config.TestJpaConfig;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+
+@DataJpaTest
+@Import(TestJpaConfig.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public @interface SuwikiJpaTest {
+}
