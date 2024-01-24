a0b5c5d10b3631a716c298ba5bf8a5179e07933c
wonseok2877 <wonseok2877@gmail.com>
2024-01-24 10:31:59 +0900

    fix: InsertNewLectureTest 전체 주석 처리

--- src/test/java/usw/suwiki/lectureutill/InsertNewLectureTest.java
+++ src/test/java/usw/suwiki/lectureutill/InsertNewLectureTest.java
@@ -1,24 +1,23 @@
-package usw.suwiki.lectureutill;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
-import usw.suwiki.domain.lecture.service.LectureService;
-
-// WARN: 통합 테스트에 포함되어선 안 됩니다.
-@SpringBootTest
-class InsertNewLectureTest {
-
-    @Autowired
-    LectureService lectureService;
-
-    @Autowired
-    LectureRepository lectureRepository;
-
-    // TODO: 파일 4개 운영 DB에 반영 -> 해당 테스트 전체 주석 처리
-    @Test
-    void insertNewLecture() {
-        lectureService.bulkSaveJsonLectures("src/main/resources/USW_2023_2.json");
-    }
-}
+//package usw.suwiki.lectureutill;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
+//import usw.suwiki.domain.lecture.service.LectureService;
+//
+//// WARN: 통합 테스트에 포함되어선 안 됩니다.
+//@SpringBootTest
+//class InsertNewLectureTest {
+//
+//    @Autowired
+//    LectureService lectureService;
+//
+//    @Autowired
+//    LectureRepository lectureRepository;
+//
+//    @Test
+//    void insertNewLecture() {
+//        lectureService.bulkSaveJsonLectures("src/main/resources/USW_2023_2.json");
+//    }
+//}
