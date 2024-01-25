65068dcfc385e6f83ad1bf187053e0be6f97e994
wonseok2877 <wonseok2877@gmail.com>
2024-01-25 18:51:04 +0900

    refactor: 테스트 프로파일 어노테이션 제거

--- src/test/java/usw/suwiki/domain/user/service/UserIsolationSchedulingServiceTestBase.java
+++ src/test/java/usw/suwiki/domain/user/service/UserIsolationSchedulingServiceTestBase.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import usw.suwiki.domain.confirmationtoken.service.ConfirmationTokenCRUDService;
 import usw.suwiki.domain.evaluatepost.controller.dto.EvaluatePostSaveDto;
@@ -37,7 +36,6 @@ import usw.suwiki.global.util.emailBuild.UserAutoDeletedWarningForm;
 import usw.suwiki.global.util.mailsender.EmailSender;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @Transactional
 public class UserIsolationSchedulingServiceTestBase extends IntegrationTestBase {
 
--- src/test/java/usw/suwiki/global/IntegrationTestBase.java
+++ src/test/java/usw/suwiki/global/IntegrationTestBase.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +24,6 @@ import usw.suwiki.global.jwt.JwtAgent;
 
 @SpringBootTest
 @Disabled
-@ActiveProfiles("test")
 @Transactional
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
