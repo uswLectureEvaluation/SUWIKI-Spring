c7b3cf7ae02c330876047cf32b948f27358e27ba
wonseok2877 <wonseok2877@gmail.com>
2024-02-08 21:36:46 +0900

    refactor: 강의 적재 로직 Disabled 처리

--- src/test/java/usw/suwiki/domain/lecture/service/InsertNewLectureTest.java
+++ src/test/java/usw/suwiki/domain/lecture/service/InsertNewLectureTest.java
@@ -1,23 +1,24 @@
-//package usw.suwiki.lectureutill;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
-//import usw.suwiki.domain.lecture.service.LectureService;
-//
-//// WARN: 통합 테스트에 포함되어선 안 됩니다.
-//@SpringBootTest
-//class InsertNewLectureTest {
-//
-//    @Autowired
-//    LectureService lectureService;
-//
-//    @Autowired
-//    LectureRepository lectureRepository;
-//
-//    @Test
-//    void insertNewLecture() {
-//        lectureService.bulkSaveJsonLectures("src/main/resources/USW_2023_2.json");
-//    }
-//}
+package usw.suwiki.domain.lecture.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import usw.suwiki.domain.lecture.domain.repository.LectureRepository;
+
+
+@SpringBootTest
+@Disabled   // WARN: 통합 테스트에 포함되어선 안 됩니다.
+class InsertNewLectureTest {
+
+    @Autowired
+    LectureService lectureService;
+
+    @Autowired
+    LectureRepository lectureRepository;
+
+    @Test
+    void insertNewLecture() {
+        lectureService.bulkApplyLectureJsonFile("src/main/resources/USW_2024_1.json");
+    }
+}
