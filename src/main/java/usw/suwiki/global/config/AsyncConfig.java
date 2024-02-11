772e572b3487ad167bee44b6bb70f267d10763b5
wonseok2877 <wonseok2877@gmail.com>
2024-02-11 21:34:45 +0900

    fix: 비동기 설정 클래스 proxyTargetClass 설정

--- src/main/java/usw/suwiki/global/config/AsyncConfig.java
+++ src/main/java/usw/suwiki/global/config/AsyncConfig.java
@@ -1,14 +1,13 @@
 package usw.suwiki.global.config;
 
+import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
-
 @Configuration
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 public class AsyncConfig implements AsyncConfigurer {
 
     @Override
