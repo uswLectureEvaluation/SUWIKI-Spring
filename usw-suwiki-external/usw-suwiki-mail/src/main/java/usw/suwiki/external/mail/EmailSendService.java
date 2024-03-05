639d86c8a934ae562f20427c9d45a93c98b601a2
Hejow <gmlwh124@naver.com>
2024-03-06 06:30:07 +0900

    refactor: external 모듈 의존성 설정

--- usw-suwiki-external/usw-suwiki-mail/src/main/java/usw/suwiki/external/mail/EmailSendService.java
+++ usw-suwiki-external/usw-suwiki-mail/src/main/java/usw/suwiki/external/mail/EmailSendService.java
@@ -5,7 +5,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import usw.suwiki.core.exception.errortype.MailException;
+import usw.suwiki.core.exception.ExceptionType;
+import usw.suwiki.core.exception.MailException;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
