d54b6df8c5aeb6a4a4e1b66a67f9224da7a2b054
Hejow <gmlwh124@naver.com>
2024-03-08 21:31:03 +0900

    refactor: 인증 api 1차 분리

--- usw-suwiki-api/usw-suwiki-client-api/src/main/java/usw/suwiki/api/auth/ConfirmationTokenControllerV2.java
+++ usw-suwiki-api/usw-suwiki-client-api/src/main/java/usw/suwiki/api/auth/ConfirmationTokenControllerV2.java
@@ -1,6 +1,4 @@
-package usw.suwiki.domain.confirmationtoken.controller;
-
-import static org.springframework.http.HttpStatus.OK;
+package usw.suwiki.api.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -10,20 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import usw.suwiki.domain.confirmationtoken.service.ConfirmationTokenBusinessService;
-import usw.suwiki.global.annotation.ApiLogger;
+import usw.suwiki.auth.token.service.ConfirmationTokenBusinessService;
+import usw.suwiki.statistics.annotation.ApiLogger;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/v2/confirmation-token")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ConfirmationTokenControllerV2 {
-
     private final ConfirmationTokenBusinessService confirmationTokenBusinessService;
 
-    @ResponseStatus(OK)
     @ApiLogger(option = "user")
     @GetMapping(value = "verify", produces = MediaType.TEXT_HTML_VALUE + ";charset=UTF-8")
+    @ResponseStatus(OK)
     public String confirmEmail(@RequestParam("token") String token) {
         return confirmationTokenBusinessService.confirmToken(token);
     }
--- usw-suwiki-api/usw-suwiki-client-api/src/main/java/usw/suwiki/api/auth/RefreshTokenControllerV2.java
+++ usw-suwiki-api/usw-suwiki-client-api/src/main/java/usw/suwiki/api/auth/RefreshTokenControllerV2.java
@@ -1,13 +1,6 @@
-package usw.suwiki.domain.refreshtoken.controller;
-
-import static org.springframework.http.HttpStatus.OK;
+package usw.suwiki.api.auth;
 
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,22 +9,28 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import usw.suwiki.domain.user.user.service.UserBusinessService;
-import usw.suwiki.global.ResponseForm;
-import usw.suwiki.global.annotation.ApiLogger;
+import usw.suwiki.common.response.ResponseForm;
+import usw.suwiki.domain.user.service.UserBusinessService;
+import usw.suwiki.statistics.annotation.ApiLogger;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/v2/refreshtoken")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RefreshTokenControllerV2 {
-
     private final UserBusinessService userBusinessService;
 
-    @Operation(summary = "Web Client 토큰 갱신")
-    @ResponseStatus(OK)
     @ApiLogger(option = "user")
+    @Operation(summary = "Web Client 토큰 갱신")
     @PostMapping("/web-client/refresh")
+    @ResponseStatus(OK)
     public ResponseForm clientTokenRefresh(
         @CookieValue(value = "refreshToken") Cookie requestRefreshCookie,
         HttpServletResponse response
@@ -39,23 +38,23 @@ public class RefreshTokenControllerV2 {
         Map<String, String> tokenPair = userBusinessService.executeJWTRefreshForWebClient(
             requestRefreshCookie
         );
+
         Cookie refreshCookie = new Cookie("refreshToken", tokenPair.get("RefreshToken"));
         refreshCookie.setMaxAge(14 * 24 * 60 * 60);
         refreshCookie.setSecure(true);
         refreshCookie.setHttpOnly(true);
         response.addCookie(refreshCookie);
+
         return ResponseForm.success(new HashMap<>() {{
             put("AccessToken", tokenPair.get("AccessToken"));
         }});
     }
 
-    @Operation(summary = "Mobile Client 토큰 갱신")
-    @ResponseStatus(OK)
     @ApiLogger(option = "user")
+    @Operation(summary = "Mobile Client 토큰 갱신")
     @PostMapping("/mobile-client/refresh")
-    public ResponseForm tokenRefresh(
-        @Valid @RequestHeader String Authorization
-    ) {
+    @ResponseStatus(OK)
+    public ResponseForm tokenRefresh(@RequestHeader String Authorization) {
         return ResponseForm.success(userBusinessService.executeJWTRefreshForMobileClient(Authorization));
     }
 }
