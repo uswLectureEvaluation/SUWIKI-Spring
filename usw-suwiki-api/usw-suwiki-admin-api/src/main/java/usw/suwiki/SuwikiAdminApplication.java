e641591947f6a478a2b78060ef50332e9a3633b0
Hejow <gmlwh124@naver.com>
2024-03-08 21:41:57 +0900

    refactor: api end-point 구성

---
+++ usw-suwiki-api/usw-suwiki-admin-api/src/main/java/usw/suwiki/SuwikiAdminApplication.java
@@ -0,0 +1,26 @@
+package usw.suwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@EnableConfigurationProperties(value = {ServerProperties.class})
+@SpringBootApplication
+public class SuwikiAdminApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SuwikiAdminApplication.class, args);
+  }
+
+  @GetMapping("/health")
+  @ResponseStatus(OK)
+  public String check() {
+    return "✅";
+  }
+}
