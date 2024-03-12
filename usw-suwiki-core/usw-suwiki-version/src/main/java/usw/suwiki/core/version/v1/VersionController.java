49dc58060f1e7d09e1886b1031c30880dee29126
Hejow <gmlwh124@naver.com>
2024-03-13 00:01:29 +0900

    refactor: 버전 모듈 의존성 추가

--- usw-suwiki-core/usw-suwiki-version/src/main/java/usw/suwiki/core/version/v1/VersionController.java
+++ usw-suwiki-core/usw-suwiki-version/src/main/java/usw/suwiki/core/version/v1/VersionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import usw.suwiki.common.response.ResponseForm;
+import usw.suwiki.domain.lecture.service.LectureCRUDService;
 
 import java.util.List;
 
