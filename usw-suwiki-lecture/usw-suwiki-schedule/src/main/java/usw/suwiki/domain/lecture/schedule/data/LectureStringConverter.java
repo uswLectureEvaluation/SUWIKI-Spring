3023c5a631a75928d273a91b5cab14a616f7aa42
Hejow <gmlwh124@naver.com>
2024-03-19 02:34:48 +0900

    refactor: 문자열 변환기 리팩토링

--- usw-suwiki-lecture/usw-suwiki-schedule/src/main/java/usw/suwiki/domain/lecture/schedule/data/LectureStringConverter.java
+++ usw-suwiki-lecture/usw-suwiki-schedule/src/main/java/usw/suwiki/domain/lecture/schedule/data/LectureStringConverter.java
@@ -1,115 +1,73 @@
 package usw.suwiki.domain.lecture.schedule.data;
 
-import usw.suwiki.domain.lecture.timetable.TimetableCellSchedule;
-import usw.suwiki.domain.lecture.timetable.TimetableDay;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
-public final class LectureStringConverter {
-
-  private static final String LOCATION_IF_BLANK = "미정";
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class LectureStringConverter {
+  private static final String LOCATION_NOT_SETTLED = "미정";
+  private static final int SPLIT_SIZE = 3;
 
   /**
-   * @param scheduleChunk 강의 장소 및 시간 원본 lecture_schedule.place_schedule
+   * @param schedule 강의 장소 및 시간 원본 lecture_schedule.place_schedule
    * @implNote place_schedule을 TimetableCellSchedule 객체 리스트로 변환
    */
-  public static List<TimetableCellSchedule> convertScheduleChunkIntoTimetableCellScheduleList(String scheduleChunk) {
-    List<TimetableCellSchedule> scheduleList = new ArrayList<>();
+  public static List<TimetableCellSchedule> chunkToTimetableCellSchedules(String schedule) {
+    List<TimetableCellSchedule> schedules = new ArrayList<>();
 
-    List<String> locationAndDaysChunkList = splitScheduleChunkIntoLocationAndDaysChunkList(scheduleChunk);
+    // e.g. "IT103(..),IT505(..)" -> [ "IT103(..)", "IT505(..)" ]
+    for (String locationAndDays : schedule.split(",(?![^()]*\\))")) {
+      String location = extractLocationFromLocationAndDays(locationAndDays);
+      String dayAndPeriodsString = extractDaysFromLocationAndDays(locationAndDays);
 
-    for (String locationAndDaysChunk : locationAndDaysChunkList) {
-      String location = resolveLocationFromLocationAndDaysChunk(locationAndDaysChunk);
+      // e.g. "월1,2, 화1,2" -> [ "월1,2", "화1,2" ]
+      for (String dayAndPeriods : dayAndPeriodsString.split(" ")) {
+        String day = dayAndPeriods.substring(0, 1);
+        String stringPeriods = dayAndPeriods.substring(1);
 
-      String DaysChunk = extractDaysChunkFromLocationAndDaysElementChunk(locationAndDaysChunk);
-      for (String dayAndPeriodsChunk : splitDaysChunkIntoDayAndPeriodsChunkList(DaysChunk)) {
-
-        String day = dayAndPeriodsChunk.substring(0, 1);
-        String periodsChunk = dayAndPeriodsChunk.substring(1);
-
-        for (String connectedPeriods : splitUnconnectedPeriodsIntoConnectedPeriodsList(periodsChunk)) {
-          List<Integer> periodList = convertStringListToIntList(connectedPeriods);
-          Integer startPeriod = periodList.get(0);
-          Integer endPeriod = periodList.get(periodList.size() - 1);
-
+        for (List<Integer> periods : splitPeriodsIntoThree(stringPeriods)) {
           TimetableCellSchedule schedule = TimetableCellSchedule.builder()
             .location(location)
             .day(TimetableDay.ofKorean(day))
-            .startPeriod(startPeriod)
-            .endPeriod(endPeriod)
+            .startPeriod(periods.get(0))
+            .endPeriod(periods.get(periods.size() - 1))
             .build();
-          scheduleList.add(schedule);
+          schedules.add(schedule);
         }
       }
     }
-    return scheduleList;
+
+    return schedules;
   }
 
-
-  private static List<String> splitScheduleChunkIntoLocationAndDaysChunkList(String chunk) {
-    // e.g. "IT103(..),IT505(..)" -> [ "IT103(..)", "IT505(..)" ]
-    return Arrays.asList(chunk.split(",(?![^()]*\\))"));
+  // e.g. "IT103(월1,2, 화1,2)" -> "IT103"
+  private static String extractLocationFromLocationAndDays(String locationAndDays) {
+    String location = locationAndDays.split("\\(")[0];
+    return location.isBlank() ? LOCATION_NOT_SETTLED : location;
   }
 
-  private static String resolveLocationFromLocationAndDaysChunk(String chunk) {
-    String locationLiteral = extractLocationFromLocationAndDaysChunk(chunk);
-    if (locationLiteral.isBlank()) {
-      return LOCATION_IF_BLANK;
-    }
-    return locationLiteral;
+  // e.g. IT103(월1,2, 화1,2) -> "월1,2, 화1,2"
+  private static String extractDaysFromLocationAndDays(String locationAndDays) {
+    int start = locationAndDays.indexOf('(') + 1;
+    int end = locationAndDays.lastIndexOf(')');
+    return locationAndDays.substring(start, end);
   }
 
-  private static String extractLocationFromLocationAndDaysChunk(String chunk) {
-    // e.g. "IT103(월1,2, 화1,2)" -> "IT103"
-    return chunk.split("\\(")[0];
-  }
-
-  private static String extractDaysChunkFromLocationAndDaysElementChunk(String chunk) {
-    // e.g. IT103(월1,2, 화1,2) -> "월1,2, 화1,2"
-    int start = chunk.indexOf('(') + 1, end = chunk.lastIndexOf(')');
-    return chunk.substring(start, end);
-  }
-
-  private static List<String> splitDaysChunkIntoDayAndPeriodsChunkList(String chunk) {
-    // e.g. "월1,2, 화1,2" -> [ "월1,2", "화1,2" ]
-    return Arrays.asList(chunk.split(" "));
-  }
-
-  private static List<String> splitUnconnectedPeriodsIntoConnectedPeriodsList(String unconnectedPeriods) {
-    // e.g. "1,2,3,5,6,7,9,10" -> [ "1,2,3", "5,6,7", "9,10" ]
-    String[] numbers = unconnectedPeriods.split(",");
-    Arrays.sort(numbers);
-
-    List<String> connectedPeriodsList = new ArrayList<>();
-    List<String> currentGroup = new ArrayList<>();
-
-    for (String number : numbers) {
-      int num = Integer.parseInt(number);
-
-      if (currentGroup.isEmpty() || num == Integer.parseInt(currentGroup.get(currentGroup.size() - 1)) + 1) {
-        currentGroup.add(number);
-      } else {
-        connectedPeriodsList.add(String.join(",", currentGroup));
-        currentGroup.clear();
-        currentGroup.add(number);
-      }
-    }
-
-    if (!currentGroup.isEmpty()) {
-      connectedPeriodsList.add(String.join(",", currentGroup));
-    }
-
-    return connectedPeriodsList;
-  }
-
-  private static List<Integer> convertStringListToIntList(String stringList) {
-    String[] elements = stringList.split(",");
-
-    return Arrays.stream(elements)
+  // e.g. "1,2,3,5,6,7,9,10" -> [ [1,2,3], [5,6,7], [9,10] ]
+  private static List<List<Integer>> splitPeriodsIntoThree(String unconnectedPeriods) {
+    List<Integer> sortedPeriods = Arrays.stream(unconnectedPeriods.split(","))
       .map(Integer::parseInt)
+      .sorted()
+      .toList();
+
+    return Stream.iterate(0, index -> index + SPLIT_SIZE)
+      .limit((sortedPeriods.size() + 2) / SPLIT_SIZE)
+      .map(current -> sortedPeriods.subList(current, Math.min(current + SPLIT_SIZE, sortedPeriods.size())))
       .toList();
   }
-
 }
